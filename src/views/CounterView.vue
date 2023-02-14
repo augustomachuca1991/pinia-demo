1ed8dd7ea56aae49af74d1c1845f1f1a375d2371
amachuca <amachuca@debian10.machuca>
2023-02-14 19:02:10 -0300

    manejo de pinia

---
+++ src/views/CounterView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>counter page: {{counterStore.counter}}</h1>
+    <button @click="increment(1)">+1</button>
+    <button @click="increment(2)">+2</button>
+    <button @click="increment(5)">+5</button>
+</template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counterStore';
+
+
+const counterStore = useCounterStore();
+
+const increment = (value : number) => {
+    counterStore.incrementBy(value);
+}
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>home page:  {{ counterStore.counter}}</h1>
+    <p> numero de cambios: {{ counterStore.numberOfChanges}}</p>
+</template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counterStore';
+const counterStore = useCounterStore();
+</script>
